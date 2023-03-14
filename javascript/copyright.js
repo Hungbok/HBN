bd08e8c09723c22e5bc2ce0240db744afecef0c8
Hungbok <bjminimn@naver.com>
2023-03-14 16:31:02 +0900

    Update

--- javascript/copyright.js
+++ javascript/copyright.js
@@ -5,6 +5,6 @@ $(document).ready(function(){
                         '</div>'+
                     '</footer>');
     $("body").append('<style>'+
-        '.footer { width: 100%; height: auto; padding-top: 100px; padding-bottom: 100px; text-align: center; font-weight: bold; color: #afafaf; background-color: #000000; /* border: 1px solid white; box-shadow: 0px 0px 5px black; -ms-user-select: none; -moz-user-select: -moz-none; -webkit-user-select: none; -khtml-user-select: none; -webkit-touch-callout: none; -webkit-user-drag: none; user-select: none; */ } .copyright { width: 80%; margin: auto; } .copyright::after { content: "Copyright 2021-2023. HungBok All Rights Reserved."; }');
+        '.footer { width: 100%; height: 250px; text-align: center; font-weight: bold; color: #cccccc; background-color: #000000; display: flex; align-content: center; justify-content: center; align-items: center; } .copyright { width: 80%; } .copyright::after { content: "Copyright 2021-2023. HungBok All Rights Reserved."; }');
         '</style>'}
 );
