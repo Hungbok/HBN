afca4d9514316c55bedabb80f7679de0fc24c329
Hungbok <bjminimn@naver.com>
2023-03-14 16:34:26 +0900

    Update

--- javascript/copyright.js
+++ javascript/copyright.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
                         '</div>'+
                     '</footer>');
     $("body").append('<style>'+
-        '.footer { width: 100%; height: 250px; text-align: center; font-weight: bold; color: #cccccc; background-color: #000000; display: flex; align-content: center; justify-content: center; align-items: center; } .copyright { width: 80%; } .copyright::after { content: "Copyright 2021-2023. HungBok All Rights Reserved."; }');
-        '</style>'}
+        '.footer { width: 100%; height: 250px; text-align: center; font-weight: bold; color: #cccccc; background-color: #000000; display: flex; align-content: center; justify-content: center; align-items: center; font-family: "Noto Sans", sans-serif; } .copyright { width: 80%; } .copyright::after { content: "Copyright 2021-2023. HungBok All Rights Reserved."; }');
+        '</style>'+
+    '<link rel="preconnect" href="https://fonts.googleapis.com">'+
+    '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>'+
+    '<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&display=swap" rel="stylesheet">'}
 );
